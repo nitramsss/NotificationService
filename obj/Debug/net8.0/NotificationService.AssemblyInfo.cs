0222f232084c828b6f3be665ccd60b8369aaec2a
fitz <jaymarnitram08@gmail.com>
2025-12-10 14:00:38 +0800

    .gitignore added

--- obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
+++ obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67189449c910226e60c3d894f913dd7d70f1b721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30fedebc09ca699ad8dcce06b5a4862c1b987d1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
